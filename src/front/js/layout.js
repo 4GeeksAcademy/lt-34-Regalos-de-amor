ea6dc5b592ff5e0dea1d08eab03f3a197f32cd2a
Bschem1818 <barbaraschem@gmail.com>
2024-11-04 23:25:30 +0000

    Fixed conde conflicts

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,31 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-import {Home} from "./pages/home";
+import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import { BeneficiaryForm } from "./pages/beneficiaryForm";
 import { Beneficiary } from "./pages/beneficiary";
 import { DonorForm } from "./component/DonorForm";
 import injectContext from "./store/appContext";
-import { Donors } from "./pages/donors"; // Asegúrate de que la ruta sea correcta
-
-
+import { Donors } from "./pages/donors";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-import List from "./pages/foundationList";
-import FoundationForm from "./pages/foundationForm";
 import { Foundation } from "./pages/foundation";
-import { DonorBeneficiary } from "./pages/donorbeneficiary";
-import { FoundationForm } from "./pages/foundationForm";
-import {PostHelper} from "./component/postHelper";
-
-
-export const Layout = () => {
+import { PostHelper } from "./component/postHelper";
 import { Login } from "./pages/login";
 import { Welcome } from "./pages/welcome";
 import { Signup } from "./component/signup";
-//create your first component
+
 const Layout = () => {
     const basename = process.env.BASENAME || "";
 
@@ -43,17 +34,13 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<Signup />} path="/signup" />
-                        <Route element={<FoundationForm />} path="/foundationForm" />
-                        <Route element={<FoundationForm />} path="/foundation/edit/:id" />
                         <Route element={<PostHelper />} path="/post" />
                         <Route element={<Welcome />} path="/welcome" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Signup />} path="/signup" />
-                        <Route element={<Beneficiary/>} path="/beneficiary" />
+                        <Route element={<Beneficiary />} path="/beneficiary" />
                         <Route element={<BeneficiaryForm />} path="/beneficiary/add" />
                         <Route element={<BeneficiaryForm />} path="/beneficiary/edit/:id" />
-                        <Route element={<DonorForm />} path="/donorform" /> 
-                        <Route element={<Donors />} path="/donors" /> 
+                        <Route element={<DonorForm />} path="/donorform" />
+                        <Route element={<Donors />} path="/donors" />
                         <Route element={<DonorForm />} path="/donorform/:id" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
