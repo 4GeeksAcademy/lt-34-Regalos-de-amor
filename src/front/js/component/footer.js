9b7f51e93aabf5e533b2a6f7254130d55bc5fdd6
Bschem1818 <barbaraschem@gmail.com>
2024-11-18 02:51:32 +0000

    updated

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,72 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="bg-dark pt-5">
+		<div className="container">
+			<div className="row g-4">
+				<div className="col-lg-3">
+					<a href="index.html">
+					</a>
+					<p className="my-3 text-body-secondary">Departure defective arranging rapturous did believe him all had supported.</p>
+					<p className="mb-2"><a href="#" className="text-body-secondary text-primary-hover"><i className="bi bi-telephone me-2"></i>+1234 568 963</a> </p>
+					<p className="mb-0"><a href="#" className="text-body-secondary text-primary-hover"><i className="bi bi-envelope me-2"></i>regalosdeamor@gmail.com</a></p>
+				</div>
+				<div className="col-lg-8 ms-auto">
+					<div className="row g-4">
+						<div className="col-6 col-md-3">
+							<h5 className="text-white mb-2 mb-md-4">Page</h5>
+							<ul className="nav flex-column text-primary-hover">
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">News and Blog</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Meet a Team</a></li>
+							</ul>
+						</div>
+						<div className="col-6 col-md-3">
+							<h5 className="text-white mb-2 mb-md-4">About us</h5>
+							<ul className="nav flex-column text-primary-hover">
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Sign up</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Sign in</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Privacy Policy</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Terms</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Cookie</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Support</a></li>
+							</ul>
+						</div>
+
+						<div className="col-6 col-md-3">
+							<h5 className="text-white mb-2 mb-md-4">Contact us</h5>
+							<ul className="nav flex-column text-primary-hover">
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Colombia</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Costa Rica</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Venezuela</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">Canada</a></li>
+								<li className="nav-item"><a className="nav-link text-body-secondary" href="#">USA</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<hr className="mt-4 mb-0" />
+			<div className="row">
+				<div className="container">
+					<div className="d-lg-flex justify-content-between align-items-center py-3 text-center text-lg-start">
+
+						<div className="text-white"> Copyrights ©2024 Booking. Build by <a href="https://www.webestica.com/" className="text-body-secondary">Webestica</a>. </div>
+
+						<div className="nav mt-2 mt-lg-0">
+							<ul className="list-inline text-primary-hover mx-auto mb-0">
+								<li className="list-inline-item me-0"><a className="nav-link text-body-secondary py-1" href="#">Privacy policy</a></li>
+								<li className="list-inline-item me-0"><a className="nav-link text-body-secondary py-1" href="#">Terms and conditions</a></li>
+								<li className="list-inline-item me-0"><a className="nav-link text-body-secondary py-1 pe-0" href="#">Refund policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 );
+
+
+
+
